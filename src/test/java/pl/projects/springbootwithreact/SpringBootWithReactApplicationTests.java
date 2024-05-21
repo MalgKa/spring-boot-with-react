b20b9e6364129a1fc04bb04a507323b73f1b2052
Gosia <kasprzak.malg@gmail.com>
2024-05-21 14:03:11 +0200

    initial commit

---
+++ src/test/java/pl/projects/springbootwithreact/SpringBootWithReactApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.projects.springbootwithreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
