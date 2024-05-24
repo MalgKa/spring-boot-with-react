70fa69e534aadfe3aae2717cf2bd553ab3539873
Gosia <kasprzak.malg@gmail.com>
2024-05-24 12:11:15 +0200

    frontend: Initialized state with useState to store contacts data, Rendered contacts list using data from state with map function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import {getContacts} from "./api/ContactService";
+import {useState} from "react";
 
 function App() {
+    const [data, setData] = useState([])
 
-    const getAllContacts =async (page=0, size=10) =>{
-        console.log(await getContacts(page,size))
+    const getAllContacts = async (page = 0, size = 10) => {
+        const {data} = await getContacts(page, size);
+        console.log(data)
+        setData(data)
     }
-  return (
-      <div>
-        <h1>so hello :)</h1>
-          <button onClick={() =>getAllContacts()}>get all contacts</button>
-      </div>
-  );
+    return (
+        <div>
+            <h1>so hello :)</h1>
+            <button onClick={() => getAllContacts()}>get all contacts</button>
+            <ul>
+                {data.content && data.content.map((contact, index) => (
+                    <li key={index}>{contact.name}, {contact.phone}, {contact.position}, {contact.email}, {contact.photo_url}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
