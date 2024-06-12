c19297a5719caa83fc78507ad753f8ff60d4fcd3
Gosia <kasprzak.malg@gmail.com>
2024-06-12 08:24:21 +0200

    frontend: Enhanced contacts display with icons, added conditional rendering of status icons to visually represent active/inactive status.

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -14,10 +14,11 @@ const Contact = ({contact}) => {
                 </div>
             </header>
             <div className='contact__body'>
-                <p>{contact.email}</p>
-                <p>{contact.phone}</p>
-                <p>{contact.address}</p>
-                <p>{contact.status}</p>
+                <p><i className="bi bi-envelope-at-fill"></i> {contact.email}</p>
+                <p><i className="bi bi-telephone-fill"></i> {contact.phone}</p>
+                <p><i className="bi bi-geo-alt-fill"></i> {contact.address}</p>
+                <p>{contact.status === "active" ? <i className="bi bi-check-circle-fill"></i> :
+                    <i className="bi bi-x-circle-fill"></i>} {contact.status}</p>
             </div>
         </Link>
     )
